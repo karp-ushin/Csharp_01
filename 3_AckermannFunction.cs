c18e5e1746fed2e41f5f2f0a6b0bf985cdcb459d
karp-ushin <108627348+karp-ushin@users.noreply.github.com>
2022-10-31 16:34:06 +0300

    Update 3_AckermannFunction.cs

--- 3_AckermannFunction.cs
+++ 3_AckermannFunction.cs
@@ -6,6 +6,8 @@ public class HelloWorld
     {
         int A(int m, int n)
         {
+            if((m<0)||(n<0)) 
+                throw new Exception("negative number(s)");
             if(m==0) return n+1;
             if(n==0) return A(m-1,1);
             return A(m-1,A(m,n-1));
