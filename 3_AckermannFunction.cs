b696cb1295aa396c062490d7232a1485f34394ee
karp-ushin <108627348+karp-ushin@users.noreply.github.com>
2022-10-31 16:20:16 +0300

    Create 3_AckermannFunction.cs

---
+++ 3_AckermannFunction.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int A(int m, int n)
+        {
+            if(m==0) return n+1;
+            if(n==0) return A(m-1,1);
+            return A(m-1,A(m,n-1));
+        }
+        const int M=3, N=2;
+        Console.WriteLine (A(M,N));
+    }
+}
