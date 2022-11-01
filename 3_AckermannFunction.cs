3b91a86a0d610fd9f6d9387d6628773e38a44efd
karp-ushin <108627348+karp-ushin@users.noreply.github.com>
2022-11-01 11:10:24 +0300

    Update 3_AckermannFunction.cs

--- 3_AckermannFunction.cs
+++ 3_AckermannFunction.cs
@@ -2,9 +2,7 @@ using System;
 
 public class HelloWorld
 {
-    public static void Main(string[] args)
-    {
-        int A(int m, int n)
+    static int A(int m, int n)
         {
             if((m<0)||(n<0)) 
                 throw new Exception("negative number(s)");
@@ -12,6 +10,8 @@ public class HelloWorld
             if(n==0) return A(m-1,1);
             return A(m-1,A(m,n-1));
         }
+    public static void Main(string[] args)
+    {
         const int M=3, N=2;
         Console.WriteLine (A(M,N));
     }
